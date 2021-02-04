ca34095c247403ccc79e276359ea71b62cf75fdb
thaumaturge <benjychada@gmail.com>
2021-02-04 15:30:54 +0000

    Minor + Old Norse vowels
    Minor updates
    1/2 of Old Norse phonetics implemented

---
+++ index.js
@@ -0,0 +1,29 @@
+let history = '';
+let phonetics_v = [];
+let phonetcs_c = [];
+let pronouns = [];
+let conjugation = false;
+let adjectives = false;
+let pre_post_postions = false;
+let sentence_structure = false;
+
+var ph_pick = document.getElementById('ph');
+ph_pick.onclick = ph();
+
+function ph() {
+    var ph = document.getElementById('phonetics');
+    var ph_val = ph.value;
+    if (ph_val == 'ol') {
+        phonetics_v = ['i', 'ĩ', 'iː', 'ĩː', 'y', 'ỹ', 'yː', 'ỹː', '', '', 'u', 'ũ', 'uː', 'ũː', 'e', 'ẽ', 'eː', 'ẽː', 'ø', 'ø̃ ', 'øː', 'ø̃ː', '', '', 'o', 'õ', 'oː', 'õː', 'ɛ', 'ɛ̃', 'ɛː', 'ɛ̃ː', 'œ', 'œ̃', '', 'a', 'ã', 'aː', 'ãː', 'ɔ', 'ɔ̃', 'ɔː', 'ɔ̃ː'];
+        
+    }
+    else if (ph_val == 'en') {
+        phonetics = 'ol';
+    }
+    else if (ph_val == 'sp') {
+        phonetics = 'ol';
+    }
+    else if (ph_val == 'fr') {
+
+    }
+}
