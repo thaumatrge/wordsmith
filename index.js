5974085c1eb60272f5bd830565894d4a2f6786f9
thaumaturge <benjychada@gmail.com>
2021-02-04 17:39:54 +0000

    Style + Minor + English Phonology
    Styled document + Minor HTML tweaks + English phonology

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ let adjectives = false;
 let pre_post_postions = false;
 let sentence_structure = false;
 
-var ph_pick = document.getElementById('ph');
-ph_pick.onclick = ph();
-
 function ph() {
     var ph = document.getElementById('phonetics');
     var ph_val = ph.value;
     if (ph_val == 'ol') {
         phonetics_v = ['i', 'ĩ', 'iː', 'ĩː', 'y', 'ỹ', 'yː', 'ỹː', '', '', 'u', 'ũ', 'uː', 'ũː', '\n','e', 'ẽ', 'eː', 'ẽː', 'ø', 'ø̃ ', 'øː', 'ø̃ː', '', '', 'o', 'õ', 'oː', 'õː', '\n', 'ɛ', 'ɛ̃', 'ɛː', 'ɛ̃ː', 'œ', 'œ̃', '', 'a', 'ã', 'aː', 'ãː', 'ɔ', 'ɔ̃', 'ɔː', 'ɔ̃ː'];
         phonetics_c = ['i','iː','y','yː',',','u','uː','\n','e','eː','ø','øː','','','o','oː','\n','ɛ','ɛː','','','a','aː','',''];
+        window.location.href = "";
     }
     else if (ph_val == 'en') {
-        phonetics = 'ol';
+        phonetics_v = ['ɪ','iː','ʊ','uː','','ɔː','\n','e','eə','ə','ɜː','ɒ','','\n','æ','','ʌ','','','ɑː','\n','eɪ','aɪ','ɔɪ','aʊ','əʊ','ɪə','ʊə','\n','(eɪə','aɪə','ɔɪə','aʊə','əʊə)'];
+        phonetics_c = ['m','','n','','','ŋ','','\n','p','','t','tʃ','','k','','\n','b','','d','dʒ','','g','','\n','f','θ','s','ʃ','','x','h','\n','v','ð','z','ʒ','','','h','\n','','','l','r','j','w',''];
     }
     else if (ph_val == 'sp') {
-        phonetics = 'ol';
+        phonetics_v = [];
+        phonetics_c = [];
     }
-    else if (ph_val == 'fr') {
-
+    else {
+        phonetics_v = [];
+        phonetics_c = [];
+        alert('Please select a valid option.')
     }
 }
