ba18d925d4e4d8deb6e0c2a971ccbd3f9363cef4
thaumatrge <benjychada@icloud.com>
2021-02-05 15:46:42 +0000

    Storage now works
    Data now persists between pages
    Took me too long to set up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var history = '';
 var pronouns = [];
+var history = '';
 var conjugation = false;
 var adjectives = false;
 var pre_post_postions = false;
@@ -7,42 +7,56 @@ var sentence_structure = false;
 
 function ph() {
     var ph_val = document.getElementById('phonetics').value;
+
     if (ph_val == 'ol') {
         sessionStorage.setItem("vowels", "'i', 'ĩ', 'iː', 'ĩː', 'y', 'ỹ', 'yː', 'ỹː', '', '', 'u', 'ũ', 'uː', 'ũː', '\n','e', 'ẽ', 'eː', 'ẽː', 'ø', 'ø̃ ', 'øː', 'ø̃ː', '', '', 'o', 'õ', 'oː', 'õː', '\n', 'ɛ', 'ɛ̃', 'ɛː', 'ɛ̃ː', 'œ', 'œ̃', '', 'a', 'ã', 'aː', 'ãː', 'ɔ', 'ɔ̃', 'ɔː', 'ɔ̃ː'");
         sessionStorage.setItem("consonants", "'i','iː','y','yː',',','u','uː','\n','e','eː','ø','øː','','','o','oː','\n','ɛ','ɛː','','','a','aː','',''");
         window.location.href = "pronouns.html";
-    }else if (ph_val == 'en') {
+    }
+    else if (ph_val == 'en') {
         sessionStorage.setItem("vowels", "'ɪ','iː','ʊ','uː','','ɔː','\n','e','eə','ə','ɜː','ɒ','','\n','æ','','ʌ','','','ɑː','\n','eɪ','aɪ','ɔɪ','aʊ','əʊ','ɪə','ʊə','\n','(eɪə','aɪə','ɔɪə','aʊə','əʊə)'");
         sessionStorage.setItem("consonants", "'m','','n','','','ŋ','','\n','p','','t','tʃ','','k','','\n','b','','d','dʒ','','g','','\n','f','θ','s','ʃ','','x','h','\n','v','ð','z','ʒ','','','h','\n','','','l','r','j','w',''");
         window.location.href = "pronouns.html";
-    }else if (ph_val == 'sp') {
+    }
+    else if (ph_val == 'sp') {
         sessionStorage.setItem("vowels", "'i','','u','\n','e','','o','\n','','a',''");
         sessionStorage.setItem("consonants", "'m','','n','ɲ','','\n','p','b','t','d','','tʃ','ʝ','k','g','\n','f','b','θ','d','s','(ʃ)','ʝ','x','g','\n','','','l','ʎ','','\n','','','ɾ','','','\n','','','r','',''");
         window.location.href = "pronouns.html";
-    }else {
+    }
+    else {
         alert('Please select a valid option.')
     }
 }
 
 function pr() {
     var pr_val = document.getElementById('pronouns').value;
-    pronouns.push([pr_val]['']);
+
+    document.getElementById('pronouns').value = "";
+
     var para = document.createElement("P");
     var input = document.createElement("INPUT");
     var br = document.createElement("BR");
+
+    pronouns.push([pr_val,""]);
+    sessionStorage.setItem('pronouns', pronouns)
+
     input.type = "text";
     input.id = String(get_unnused(pronouns));
+
     para.style = "display: inline-block";
     input.style = "display: inline-block; border-top: none; border-left: none; border-right: none; font-family: 'IM FELL DW Pica'; border-radius: 0px; letter-spacing: 1px; font-size: 15px; position: relative; left: 1%";
+
     input.innerHtml = "";
     para.innerHTML = pr_val + ' ';
+
     document.getElementById("main").appendChild(para);
     document.getElementById("main").appendChild(input);
     document.getElementById("main").appendChild(br);
-    alert(pronouns.length);
 }
 
 function pr_submit() {
+    pronouns = sessionStorage.getItem('pronouns').split(",")
+
     for (i = 0; i < pronouns.length; i++) {
         alert(i);
         var el = document.getElementById(i).value;
